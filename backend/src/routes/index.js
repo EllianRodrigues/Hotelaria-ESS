9c80d530dc8e4c9b79b6b7021d904c5b51979856
Guilherme Xavier <guilherme.xavier@incognia.com>
2025-06-29 16:40:57 -0300

    chore: delete unnecessary comment

--- backend/src/routes/index.js
+++ backend/src/routes/index.js
@@ -2,7 +2,6 @@ const express = require('express');
 const router = express.Router();
 const userController = require('../controllers/userController');
 
-// Example route
 router.get('/users', userController.getAllUsers);
 
 module.exports = router; 
